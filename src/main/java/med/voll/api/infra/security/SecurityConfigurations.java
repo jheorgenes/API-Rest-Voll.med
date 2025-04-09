35855cf17776fc6a88097835d8c244858401a021
Jheorgenes Warlley <jheorgenes@gmail.com>
2025-04-09 19:44:26 -0300

    - Criando Entidade de Usuários - Implementando o Spring Security - Criando arquivo de configuração SecurityConfigurations para definir nivel de segurança para STATELESS - Criando serviço de Autenticacao que implementa UserDetailsService - Criando Repository de Usuário para fazer a busca no banco de dados pelo username recuperado.

---
+++ src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -0,0 +1,21 @@
+package med.voll.api.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity // Habilita personalização de segurança do Spring
+public class SecurityConfigurations {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) //Mudando de STATEFUL para STATELESS
+                .build();
+    }
+}
