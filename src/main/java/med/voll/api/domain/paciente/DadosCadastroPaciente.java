fdada56366c765fb9f1db2ce3752d0312c7255aa
Jheorgenes Warlley <jheorgenes@gmail.com>
2025-04-09 01:46:45 -0300

    - Ajuste de pacotes

--- src/main/java/med/voll/api/domain/paciente/DadosCadastroPaciente.java
+++ src/main/java/med/voll/api/domain/paciente/DadosCadastroPaciente.java
@@ -1,6 +1,5 @@
-package med.voll.api.paciente;
+package med.voll.api.domain.paciente;
 
-import med.voll.api.endereco.DadosEndereco;
-import med.voll.api.endereco.Endereco;
+import med.voll.api.domain.endereco.DadosEndereco;
 
 public record DadosCadastroPaciente(String nome, String email, String telefone, String cpf, DadosEndereco endereco) { }
